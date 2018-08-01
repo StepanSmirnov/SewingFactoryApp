b2628c7007bad6e53679f69f1c9bb74a6e4372ca
sssprogramer <sssprogramer@gmail.com>
2018-08-01 17:15:02 +0300

    remove debug code

--- SewingFactory/Forms/CreateOrderForm.cs
+++ SewingFactory/Forms/CreateOrderForm.cs
@@ -72,24 +72,7 @@ namespace SawingFactory.Forms
                     }
                 }
                 context.Orders.Add(order);
-                try
-                {
-                    context.SaveChanges();
-                }
-                catch (DbEntityValidationException ex)
-                {
-                    foreach (var eve in ex.EntityValidationErrors)
-                    {
-                        Console.WriteLine("Entity of type \"{0}\" in state \"{1}\" has the following validation errors:",
-                            eve.Entry.Entity.GetType().Name, eve.Entry.State);
-                        foreach (var ve in eve.ValidationErrors)
-                        {
-                            Console.WriteLine("- Property: \"{0}\", Error: \"{1}\"",
-                                ve.PropertyName, ve.ErrorMessage);
-                        }
-                    }
-                    throw;
-                }
+                context.SaveChanges();
             }
         }
 
